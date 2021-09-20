6952a5ef85f6564c05e95a04b05a1cb05400059c
momopinata <79038235+momopinata@users.noreply.github.com>
2021-09-19 23:54:52 -0700

    Add files via upload

---
+++ 2_1_Sulaiman_Arsalai/js/script.js
@@ -0,0 +1,12 @@
+	$(".complicated dt").on("click",function(){
+  
+  		let id = $(this).index(".complicated dt");
+  		let nextSibling = $(".complicated dd").eq(id);
+
+  		if(nextSibling.is(":visible")) {
+    	nextSibling.slideUp();
+  		} else {
+    		nextSibling.slideDown()
+      			.siblings("dd").slideUp();
+  		}
+	})
